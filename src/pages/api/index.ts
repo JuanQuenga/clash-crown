a2d0c4c607b9fc31a0486993db64f5b796357d25
Juan Quenga <juanquenga@gmail.com>
2022-09-25 14:37:37 -0400

    BaseURL ENV for ClashRoyale API

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,5 @@
 import { WrapRoyaleCore } from "../../util/WrapRoyaleCore";
 
 export const WrapRoyale = new WrapRoyaleCore(
-	process.env.REACT_APP_CLASH_ROYALE_API_TOKEN as string
+	process.env.REACT_APP_CLASH_ROYALE_API_TOKEN as string, process.env.REACT_APP_CLASH_ROYALE_API_BASE_URL as string
 );
